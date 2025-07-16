ea3e0f654d8f0d0da75ef6f8df2703c1566dff22
ArpanParajuli <arpanparajuli394@gmail.com>
2025-07-16 16:39:11 +0545

    Add project files.

---
+++ SimpleBankAccount/Bank.cs
@@ -0,0 +1,208 @@
+﻿    using System;
+using System.CodeDom;
+using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    namespace SimpleBankAccount
+    {
+        public partial class Bank: Form
+        {
+        List<Customer> Customers = new List<Customer>();
+        public Bank()
+            {
+              InitializeComponent();
+            }
+
+            private void button1_Click(object sender, EventArgs e) // create account
+            {
+
+            if (float.TryParse(textBox3.Text, out float amount))
+            {
+                try
+                {
+
+                    foreach(var customer in Customers)
+                    {
+                        if(customer.Customer_name == textBox3.Text)
+                        {
+                            throw new Exception("Customer Account ID already exists");
+                        }
+
+                        if (customer.Customer_name == textBox1.Text)
+                        {
+                            throw new Exception("Customer name already exists");
+                        }
+                    }
+
+                    if (amount >= 0 && amount <= 100000)
+                    {
+                        Customer c = new Customer(textBox1.Text, textBox2.Text, amount);
+                        Customers.Add(c);
+                        MessageBox.Show("Account created successfully");
+                        textBox7.Text += "Account created : " + c.Customer_name + " : " + c.accountNumber + Environment.NewLine;
+                    }
+
+                    else
+                    {
+                        throw new Exception("Amount input exceed");
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                   MessageBox.Show(ex.Message);
+                }
+
+            }
+
+            else
+            {
+                MessageBox.Show("Please enter a valid number for age.");
+            }
+            }
+
+            private void Bank_Load(object sender, EventArgs e)
+            {
+               
+            }
+
+            private void textBox2_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void textBox3_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void textBox1_TextChanged(object sender, EventArgs e) // name
+            {
+
+            }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show("Enter valid Account number");
+            }
+
+            foreach (var customer in Customers)
+            {
+                if (textBox4.Text == customer.accountNumber)
+                {
+                    textBox6.Text = customer.Balance.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect account number!");
+                }
+
+            }
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show("Enter valid Account number");
+            }
+
+            foreach (var customer in Customers)
+            {
+                if (textBox4.Text == customer.accountNumber)
+                {
+                    if ( float.TryParse(textBox5.Text, out float amount))
+                    {
+                       await customer.DepositAsync(amount);
+                        textBox7.Text += "Amount Deposited  : " + customer.Customer_name + " : " + customer.accountNumber + " | " + amount  + Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect account number!");
+                }
+
+            }
+        }
+
+        private void textBox6_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show("Enter valid Account number");
+            }
+
+            foreach (var customer in Customers)
+            {
+                if (textBox4.Text == customer.accountNumber)
+                {
+                    if(float.TryParse(textBox5.Text , out float amount)){
+                        await customer.WithdrawAsync(amount);
+                        textBox7.Text += "Amount Withdraw  : " + customer.Customer_name + " : " + customer.accountNumber + " | " + amount + Environment.NewLine;
+
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Incorrect account number!");
+                }
+
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
---
+++ SimpleBankAccount/BankSystem/Customer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+
+class Customer
+    {
+        public string Customer_name { get; set; }
+
+        public string accountNumber { get; set; }
+
+        public float Balance { get; set;}
+
+
+    public Customer(string name , string accountNumber , float Balance)
+    {
+        this.Customer_name = name;
+        this.accountNumber = accountNumber;
+        this.Balance = Balance;
+    }
+
+
+        public async Task DepositAsync(float amount)
+        {
+
+        try
+        {
+            await Task.Delay(1000);
+            if (amount >= 0 && amount <= 100000) // 1 lakh per one time
+            {
+
+                if (amount >= 1 && amount <= 100000) // 10lakhs
+                {
+                    this.Balance += amount;
+                }
+
+                else
+                {
+                    throw new Exception("Invalid Amount");
+                }
+
+            }
+
+            else
+            {
+                throw new Exception("Deposit Amount is more!");
+            }
+                
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+            
+        }
+
+        public async Task WithdrawAsync(float amount)
+        {
+        try
+        {
+
+
+
+           await Task.Delay(1000);
+
+            if(amount >= 0 && amount <= 100000) // 1 lakh per one time
+            {
+                if (amount >= 0 && amount <= this.Balance) // 10lakhs
+                {
+                    this.Balance -= amount;
+                }
+
+                else
+                {
+                    throw new Exception("Invalid Amount");
+                }
+            }
+
+
+            else
+            {
+                throw new Exception("Withdrawn Amount is more!");
+            }
+          
+
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+        }
+    }
+
